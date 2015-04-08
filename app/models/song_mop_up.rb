df48118aa0c75676acb2ebf8a753684c7ad31eab
Andy Weiss <andyweiss1982@gmail.com>
2015-04-07 23:39:42 -0400

    comment out mop up

--- app/models/song_mop_up.rb
+++ app/models/song_mop_up.rb
@@ -1,40 +1,40 @@
-class SongMopUp
-  require 'nokogiri'
-  require 'open-uri'
+# class SongMopUp
+#   require 'nokogiri'
+#   require 'open-uri'
 
-  do_overs = []
+#   do_overs = []
 
-  BillboardDate.all.each do |bbd|
-    if bbd.songs.count < 1
-      do_overs << bbd
-    end
-  end
+#   BillboardDate.all.each do |bbd|
+#     if bbd.songs.count < 1
+#       do_overs << bbd
+#     end
+#   end
 
-  do_overs.each do |bbd|
+#   do_overs.each do |bbd|
 
-    date_string = bbd.day
+#     date_string = bbd.day
 
-    url = "https://www.billboard.com/charts/r-b-hip-hop-songs/"
+#     url = "https://www.billboard.com/charts/r-b-hip-hop-songs/"
 
-    search_url = url + date_string + '/'
+#     search_url = url + date_string + '/'
 
-    begin
+#     begin
     
-    doc = Nokogiri::HTML(open(search_url))  
-    number_1_song = doc.at_css('#row-1 > div.row-primary > div.row-title > h2').text.lstrip.rstrip
-    number_1_artist = doc.at_css('#row-1 > div.row-primary > div.row-title > h3 > a').text.lstrip.rstrip
+#     doc = Nokogiri::HTML(open(search_url))  
+#     number_1_song = doc.at_css('#row-1 > div.row-primary > div.row-title > h2').text.lstrip.rstrip
+#     number_1_artist = doc.at_css('#row-1 > div.row-primary > div.row-title > h3 > a').text.lstrip.rstrip
 
-    Song.create(name: number_1_song, artist: number_1_artist, date: date_string, billboard_id: bbd.id)
+#     Song.create(name: number_1_song, artist: number_1_artist, date: date_string, billboard_id: bbd.id)
     
-    rescue
+#     rescue
 
-      puts "==================================================="
-      puts "#{date_string} BAD GATEWAY BillboardDate.#{bbd.id}"
-      puts "==================================================="
+#       puts "==================================================="
+#       puts "#{date_string} BAD GATEWAY BillboardDate.#{bbd.id}"
+#       puts "==================================================="
     
-    end
+#     end
     
-    sleep 2
+#     sleep 2
 
-  end
-end
+#   end
+# end
