6cddb52e4f980a47c09aaa00cee3aa04302ee00e
Andy Weiss <andyweiss1982@gmail.com>
2015-04-07 20:14:01 -0400

    okok

--- app/models/song_importer.rb
+++ app/models/song_importer.rb
@@ -1,26 +1,26 @@
-class SongImporter
-  require 'nokogiri'
-  require 'open-uri'
-  # require 'httparty'
+# class SongImporter
+#   require 'nokogiri'
+#   require 'open-uri'
+#   # require 'httparty'
   
-  url = "https://www.billboard.com/charts/r-b-hip-hop-songs/"
+#   url = "https://www.billboard.com/charts/r-b-hip-hop-songs/"
 
-  b = BillboardDate.first
+#   b = BillboardDate.first
 
-  search_url = url + b.day.to_s
+#   search_url = url + b.day.to_s
 
-  puts get(search_url)
+#   puts get(search_url)
 
-  # doc = Nokogiri::HTML(search_url)
+#   # doc = Nokogiri::HTML(search_url)
 
-  # doc = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
+#   # doc = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
 
-  # puts doc
+#   # puts doc
 
-#   doc.xpath('//*[@id="row-1"]/div[1]/div[5]/h2').each do |link|
-#   puts link.content
+# #   doc.xpath('//*[@id="row-1"]/div[1]/div[5]/h2').each do |link|
+# #   puts link.content
+# # end
+
+  
+
 # end
-
-  
-
-end
