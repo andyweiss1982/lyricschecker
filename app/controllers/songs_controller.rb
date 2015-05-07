ee3f73f578ba58d8f1552d9c5e42ee2e84c3bb75
Andy Weiss <andyweiss1982@gmail.com>
2015-05-07 00:00:20 -0400

    new inst vars

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,104 +8,67 @@ class SongsController < ApplicationController
   end
 
   def total
-    @songs = Song.all
-    @songs_1985 = []
-    @songs_1986 = []
-    @songs_1987 = []
-    @songs_1988 = []
-    @songs_1989 = []
-    @songs_1990 = []
-    @songs_1991 = []
-    @songs_1992 = []
-    @songs_1993 = []
-    @songs_1994 = []
-    @songs_1995 = []
-    @songs_1996 = []
-    @songs_1997 = []
-    @songs_1998 = []
-    @songs_1999 = []
-    @songs_2000 = []
-    @songs_2001 = []
-    @songs_2002 = []
-    @songs_2003 = []
-    @songs_2004 = []
-    @songs_2005 = []
-    @songs_2006 = []
-    @songs_2007 = []
-    @songs_2008 = []
-    @songs_2009 = []
-    @songs_2010 = []
-    @songs_2011 = []
-    @songs_2012 = []
-    @songs_2013 = []
-    @songs_2014 = []
-    @songs_2015 = []
+    @songs_1985 = Song.where(year: 1985)
 
-    @songs.all.each do |song|
-      if song.year == 1985
-        @songs_1985 << song
-      elsif song.year == 1986
-        @songs_1986 << song
-      elsif song.year == 1987
-        @songs_1987 << song
-      elsif song.year == 1988
-        @songs_1988 << song
-      elsif song.year == 1989
-        @songs_1989 << song
-      elsif song.year == 1990
-        @songs_1990 << song
-      elsif song.year == 1991
-        @songs_1991 << song
-      elsif song.year == 1992
-        @songs_1992 << song
-      elsif song.year == 1993
-        @songs_1993 << song
-      elsif song.year == 1994
-        @songs_1994 << song
-      elsif song.year == 1995
-        @songs_1995 << song
-      elsif song.year == 1996
-        @songs_1996 << song
-      elsif song.year == 1997
-        @songs_1997 << song
-      elsif song.year == 1998
-        @songs_1998 << song
-      elsif song.year == 1999
-        @songs_1999 << song
-      elsif song.year == 2000
-        @songs_2000 << song
-      elsif song.year == 2001
-        @songs_2001 << song
-      elsif song.year == 2002
-        @songs_2002 << song
-      elsif song.year == 2003
-        @songs_2003 << song
-      elsif song.year == 2004
-        @songs_2004 << song
-      elsif song.year == 2005
-        @songs_2005 << song
-      elsif song.year == 2006
-        @songs_2006 << song
-      elsif song.year == 2007
-        @songs_2007 << song
-      elsif song.year == 2008
-        @songs_2008 << song
-      elsif song.year == 2009
-        @songs_2009 << song
-      elsif song.year == 2010
-        @songs_2010 << song
-      elsif song.year == 2011
-        @songs_2011 << song
-      elsif song.year == 2012
-        @songs_2012 << song
-      elsif song.year == 2013
-        @songs_2013 << song
-      elsif song.year == 2014
-        @songs_2014 << song
-      elsif song.year == 2015
-        @songs_2015 << song
-      end
-    end
+    @songs_1986 = Song.where(year: 1986)
+
+    @songs_1987 = Song.where(year: 1987)
+
+    @songs_1988 = Song.where(year: 1988)
+
+    @songs_1989 = Song.where(year: 1989)
+
+    @songs_1990 = Song.where(year: 1990)
+
+    @songs_1991 = Song.where(year: 1991)
+
+    @songs_1992 = Song.where(year: 1992)
+
+    @songs_1993 = Song.where(year: 1993)
+
+    @songs_1994 = Song.where(year: 1994)
+
+    @songs_1995 = Song.where(year: 1995)
+
+    @songs_1996 = Song.where(year: 1996)
+
+    @songs_1997 = Song.where(year: 1997)
+  
+    @songs_1998 = Song.where(year: 1998)
+  
+    @songs_1999 = Song.where(year: 1999)
+  
+    @songs_2000 = Song.where(year: 2000)
+  
+    @songs_2001 = Song.where(year: 2001)
+  
+    @songs_2002 = Song.where(year: 2002)
+  
+    @songs_2003 = Song.where(year: 2003)
+  
+    @songs_2004 = Song.where(year: 2004)
+  
+    @songs_2005 = Song.where(year: 2005)
+  
+    @songs_2006 = Song.where(year: 2006)
+  
+    @songs_2007 = Song.where(year: 2007)
+  
+    @songs_2008 = Song.where(year: 2008)
+  
+    @songs_2009 = Song.where(year: 2009)
+  
+    @songs_2010 = Song.where(year: 2010)
+  
+    @songs_2011 = Song.where(year: 2011)
+  
+    @songs_2012 = Song.where(year: 2012)
+  
+    @songs_2013 = Song.where(year: 2013)
+  
+    @songs_2014 = Song.where(year: 2014)
+  
+    @songs_2015 = Song.where(year: 2015)
   end
 
   # GET /songs/1
